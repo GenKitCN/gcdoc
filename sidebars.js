57dd354d480dda1c00b82a6ce077036f4529132b
Wansn <49867372+Wansn-w@users.noreply.github.com>
2022-05-01 20:14:22 +0800

    Update sidebars.js

--- sidebars.js
+++ sidebars.js
@@ -1,6 +1,7 @@
 module.exports = {
   someSidebar: {
-    Docusaurus: ['doc1', 'doc2', 'doc3'],
-    Features: ['mdx'],
+    "快速开始": ['doc1', 'doc2', 'doc3'],
+    "在其他平台上运行": ['mdx'],
+    "FAQ": ['mdx'],
   },
 };
