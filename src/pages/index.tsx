b8cb9f0753d109ebb8bc44f491f5130badaaec86
Chi_Tang <bilibilichitang@hotmail.com>
2022-05-11 21:57:48 +0800

    added title in head

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -29,8 +29,8 @@ export default function Home(): JSX.Element {
   const {siteConfig} = useDocusaurusContext();
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
+      title={`GenKit Wiki`}
+      description="Grasscutter 中文百科">
       <HomepageHeader />
       <main>
         <HomepageFeatures />
