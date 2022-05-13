257609e8168cdc1f7b02b7f9b1078f0f3fc0e28b
Wansn <2632324507@qq.com>
2022-05-13 12:15:49 +0800

    something change
    - OPT load speed
    - REMOVE other lang
    - MOD artifact lvl input
    - MOD artifact commands
    
    *Note: artifact page still have issue(can't load data sometimes), i'll rewrite it instead of modify from exzork/gc-tools

--- src/pages/artifact.tsx
+++ src/pages/artifact.tsx
@@ -30,9 +30,6 @@ interface IAffixOpt {
 }
 
 export default function Artifacts() {
-    const [reliquaries, setReliquary] = useState<IReliquary[]>([]);
-    const [reliquaryMains, setReliquaryMains] = useState<IReliquaryMain[]>([]);
-    const [reliquaryAffixes, setReliquaryAffixes] = useState<IReliquaryAffix>({});
     const [uid, setUid] = useState(10001);
     const [selectedArtifact, setSelectedArtifact] = useState(0);
     const [selectedMainStat, setSelectedMainStat] = useState(0);
@@ -45,74 +42,24 @@ export default function Artifacts() {
     const [artifactAffixes, setArtifactAffixes] = useState<{[key:string]:IAffixOpt[]}>({});
     const [generatedArtifact, setGeneratedArtifact] = useState("/giveart ");
 
-    const [TextMap, setTextMap] = useState({});
-    const affix2text = {
-        "FIGHT_PROP_HP": 2404061249,
-        "FIGHT_PROP_ATTACK": 1756301290,
-        "FIGHT_PROP_DEFENSE": 1575853882,
-        "FIGHT_PROP_SUB_HURT": 3892469682,
-        "FIGHT_PROP_HEAL_ADD": 3911103831,
-        "FIGHT_PROP_CRITICAL": 1916797986,
-        "FIGHT_PROP_ADD_HURT": 2920451417,
-        "FIGHT_PROP_HP_PERCENT": 2756497090,
-        "FIGHT_PROP_HEALED_ADD": 3392845215,
-        "FIGHT_PROP_ICE_SUB_HURT": 2221516638,
-        "FIGHT_PROP_ICE_ADD_HURT": 4054347456,
-        "FIGHT_PROP_WIND_ADD_HURT": 312842903,
-        "FIGHT_PROP_ROCK_SUB_HURT": 3363551926,
-        "FIGHT_PROP_ELEC_SUB_HURT": 1580778495,
-        "FIGHT_PROP_CRITICAL_HURT": 4137936461,
-        "FIGHT_PROP_SPEED_PERCENT": 3552803520,
-        "FIGHT_PROP_WIND_SUB_HURT": 675300986,
-        "FIGHT_PROP_ROCK_ADD_HURT": 2557985416,
-        "FIGHT_PROP_ELEC_ADD_HURT": 3514877774,
-        "FIGHT_PROP_FIRE_SUB_HURT": 570007818,
-        "FIGHT_PROP_FIRE_ADD_HURT": 999734248,
-        "FIGHT_PROP_ATTACK_PERCENT": 2796372974,
-        "FIGHT_PROP_GRASS_SUB_HURT": 2809766539,
-        "FIGHT_PROP_GRASS_ADD_HURT": 1824382851,
-        "FIGHT_PROP_WATER_ADD_HURT": 3619239513,
-        "FIGHT_PROP_WATER_SUB_HURT": 1406081734,
-        "FIGHT_PROP_DEFENSE_PERCENT": 3076885710,
-        "FIGHT_PROP_ELEMENT_MASTERY": 2326066533,
-        "FIGHT_PROP_PHYSICAL_ADD_HURT": 3763864883,
-        "FIGHT_PROP_PHYSICAL_SUB_HURT": 2073208032,
-        "FIGHT_PROP_CHARGE_EFFICIENCY": 1735465728,
-        "FIGHT_PROP_SKILL_CD_MINUS_RATIO": 1083556685,
-        "FIGHT_PROP_SHIELD_COST_MINUS_RATIO": 195785769
-    }
 
     useEffect(() => {
         const dataArtifact: IOption[] = [];
         const dataMainAttrs: IOption[] = [];
         const dataAffixes: {[key:string]:IAffixOpt[]} = {}
-        const initReliquaryData = async () => {
-            await ReliquaryDataProvider.init();
-            setReliquary(ReliquaryDataProvider.getReliquary());
-            setReliquaryMains(ReliquaryDataProvider.getReliquaryMains());
-            setReliquaryAffixes(ReliquaryDataProvider.getReliquaryAffixes());
-        };
-        initReliquaryData();
-        fetch("https://raw.githubusercontent.com/Dimbreath/GenshinData/master/TextMap/TextMapCHS.json")
+        fetch("/FightProp.json")
             .then(resp => resp.json())
-            .then(textMap => {
+            .then(fightProp => {
                 let ReliquaryAffixes = ReliquaryDataProvider.getReliquaryAffixes();
-                ReliquaryDataProvider.getReliquary().forEach(reliquary => {
-                    const id = reliquary.id;
-                    const name = textMap[reliquary.nameTextMapHash];
-                    if (dataArtifact.filter((x) => x.name === name).length === 0) {
-                        dataArtifact.push({id: id, name: name});
-                    }
-                });
                 ReliquaryDataProvider.getReliquaryMains().forEach(mainAttr => {
                     const id = mainAttr.id;
-                    const name = textMap[affix2text[mainAttr.propType]];
+                    const name = fightProp[`cmdGen.Artifact.${mainAttr.propType}`];
                     dataMainAttrs.push({id: id, name: name});
                 });
                 Object.keys(ReliquaryAffixes).map((key, index) => {
                     ReliquaryAffixes[key].map((affix, rIndex) => {
                         const id = affix.id;
-                        const name = textMap[affix2text[affix.propType]];
+                        const name = fightProp[`cmdGen.Artifact.${affix.propType}`];
                         const index = affix.index;
                         const value = affix.propValue;
                         const displayValue = getPercent(affix);
@@ -122,12 +69,24 @@ export default function Artifacts() {
                         dataAffixes[name].push({id: id, name: name, index: index, value: value, displayValue: displayValue})
                     });
                 });
-                setArtifactData(dataArtifact);
                 setArtifactMains(dataMainAttrs);
                 setArtifactAffixes(dataAffixes);
+                }
+            );
+        fetch("/SimpleTextMap.json")
+            .then(resp => resp.json())
+            .then(textMap => {
+                ReliquaryDataProvider.getReliquary().forEach(reliquary => {
+                    console.log(reliquary)
+                    const id = reliquary.id;
+                    const name = textMap[`cmdGen.TextMap.${reliquary.nameTextMapHash}`];
+                    if (dataArtifact.filter((x) => x.name === name).length === 0) {
+                        dataArtifact.push({id: id, name: name});
+                    }
+                });
+                setArtifactData(dataArtifact);
             });
-    }, []);
-
+    },[]);
     useEffect(() => {
         handleGeneratedArtifact()
     }, [uid, selectedArtifact, selectedMainStat, selectedAffixes, selectedAffixesAmount, artifactEnhancements]);
@@ -202,59 +161,63 @@ export default function Artifacts() {
                         margin: 'auto',
                         maxWidth: 1000,
                         flexGrow: 1,
-                        background: "rgb(240, 240, 240)"
+                        background: "rgb(253, 253, 253)"
                     }}
                 >
                     <Grid container spacing={2} columns={{
                         xs: 1,
                         sm: 2
                     }}>
-                        <Grid item container xs={12} sm={5}>
-                            <Grid item container>
-                                <Grid item xs={10}>
-                                    <TextField
-                                        required
-                                        id="outlined-required"
-                                        label="UID"
-                                        defaultValue="10001"
-                                        onChange={(event) => setUid(parseInt(event.target.value))}
+                        <Grid item container xs={12} sm={5} spacing={2}>
+                            <Grid item container spacing={2}>
+                                <Grid item xs={9}>
+                                    <TextField fullWidth
+                                               required
+                                               id="outlined-required"
+                                               label="UID"
+                                               defaultValue="10001"
+                                               onChange={(event) => setUid(parseInt(event.target.value))}
                                     />
                                 </Grid>
-                                <Grid item xs={2}>
-                                    <Input
-                                        value={artifactEnhancements}
-                                        size="small"
-                                        onChange={(e)=>setArtifactEnhancements(Number(e.currentTarget.value))}
-                                        inputProps={{
-                                            step: 1,
-                                            min: 0,
-                                            max: 20,
-                                            type: 'number'
-                                        }}
-                                    />
+                                <Grid item xs={3}>
+                                    <TextField fullWidth
+                                               required
+                                               id="outlined-required"
+                                               label="等级"
+                                               defaultValue={artifactEnhancements}
+                                               inputProps={{ inputMode: 'numeric',
+                                                   step: 1,
+                                                   min: 0,
+                                                   max: 20,
+                                                   type: 'number'}}
+                                               onChange={(e)=>setArtifactEnhancements(Number(e.currentTarget.value))}/>
                                 </Grid>
                             </Grid>
-                            <Autocomplete
-                                aria-label="Artifact Name" id="ArtifactName"
-                                options={artifactData}
-                                sx={{
-                                    width: "inherit"
-                                }}
-                                getOptionLabel={(option) => option.name}
-                                onChange={handleArtifactChange}
-                                renderInput={(params) => <TextField {...params} label="圣遗物名" variant="outlined"/>}
-                            />
-                            <Autocomplete
-                                aria-label="Artifact Main Stats" id="ArtifactMainStats"
-                                options={artifactMains}
-                                sx={{
-                                    width: "inherit"
-                                }}
-                                getOptionLabel={(option) => option.name}
-                                onChange={handleMainStatChange}
-                                renderInput={(params) => <TextField {...params} label="主属性"
-                                                                    variant="outlined"/>}
-                            />
+                            <Grid item xs={12}>
+                                <Autocomplete
+                                    aria-label="Artifact Name" id="ArtifactName"
+                                    options={artifactData}
+                                    sx={{
+                                        width: "inherit"
+                                    }}
+                                    getOptionLabel={(option) => option.name}
+                                    onChange={handleArtifactChange}
+                                    renderInput={(params) => <TextField {...params} label="圣遗物名" variant="outlined"/>}
+                                />
+                            </Grid>
+                            <Grid item xs={12}>
+                                <Autocomplete
+                                    aria-label="Artifact Main Stats" id="ArtifactMainStats"
+                                    options={artifactMains}
+                                    sx={{
+                                        width: "inherit"
+                                    }}
+                                    getOptionLabel={(option) => option.name}
+                                    onChange={handleMainStatChange}
+                                    renderInput={(params) => <TextField {...params} label="主属性"
+                                                                        variant="outlined"/>}
+                                />
+                            </Grid>
                         </Grid>
                         <Grid item container xs={12} sm={7}>
                             <List
@@ -303,7 +266,9 @@ export default function Artifacts() {
                                 ))}
                             </List>
                         </Grid>
-                        <TextField fullWidth label="命令 (20级的等级为21)" variant="standard" value={generatedArtifact}/>
+                        <Grid item xs={12}>
+                            <TextField fullWidth label="命令 (20级的等级为21)" variant="standard" value={generatedArtifact}/>
+                        </Grid>
                     </Grid>
                 </Paper>
             </Container>
